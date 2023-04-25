cc37fa28d018c1fc14ce41b60c6da7837c354c39
samgbsr <sam.garcia.96@gmail.com>
2023-04-24 21:57:23 -0600

    Primeros endpoints completos Se implementan los primeros gets para probar funcionalidad del front mas adelante.

--- API/server/index.js
+++ API/server/index.js
@@ -22,7 +22,7 @@ app.get("/test", (req, res) => {
     res.json({ message: "Hello from server side!" });
 });
 
-app.get("/petitions/pending", async (req, res) => {
+app.get("/dashboard/pending", async (req, res) => {
     try {
         const connection = await pool.getConnection();
         const [rows] = await connection.execute("CALL sp_get_pending_petitions() ");
@@ -35,7 +35,7 @@ app.get("/petitions/pending", async (req, res) => {
     }
 });
 
-app.get("/petitions/notPending", async (req, res) => {
+app.get("/dashboard/notPending", async (req, res) => {
     try {
         const connection = await pool.getConnection();
         const [rows] = await connection.execute("CALL sp_get_not_pending_petitions() ");
@@ -48,7 +48,7 @@ app.get("/petitions/notPending", async (req, res) => {
     }
 });
 
-app.get('/petitions/user/:id', async (req, res) => {
+app.get('/user/:id', async (req, res) => {
     const { id } = req.params;
   
     try {
@@ -61,6 +61,20 @@ app.get('/petitions/user/:id', async (req, res) => {
       console.error(err);
       res.status(500).send('Error retrieving data from the database');
     }
-  });
+});
 
+app.get('/petition/:id/comment', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const connection = await pool.getConnection();
+      const [rows] = await connection.execute('CALL get_arco_comment(?)', [id]);
+      connection.release();
+      res.json(rows);
+
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error retrieving data from the database');
+    }
+});
 
