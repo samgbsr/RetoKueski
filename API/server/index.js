0d77213c178b226c1477f9ec72cd128f66c9f626
samgbsr <samgbsr@gmail.com>
2023-04-25 20:28:25 -0600

    2

--- API/server/index.js
+++ API/server/index.js
@@ -89,6 +89,22 @@ app.put('/user/:id/opposition', async (req, res) => {
 });
 
 //endpoints PETITION
+
+app.get('/petition/:id/', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.execute('CALL get_arco_petition_info(?)', [id]);
+        connection.release();
+        res.json(rows[0]);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving data from the database');
+    }
+});
+
 app.get('/petition/:id/comment', async (req, res) => {
     const { id } = req.params;
 
