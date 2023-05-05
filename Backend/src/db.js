c6ffc89b495718a43f90fe0b3ed679916d618904
samgbsr <sam.garcia.96@gmail.com>
2023-05-04 19:45:39 -0600

    Db Fix

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -2,7 +2,7 @@ import { createPool } from 'mysql2/promise.js'
 import { MYSQLDATABASE, MYSQLHOST, MYSQLPASSWORD, MYSQLUSER} from './config.js'
 
 //BD
-export const pool = mysql.createPool({
+export const pool = createPool({
     host: MYSQLHOST,
     user: MYSQLUSER,
     password: MYSQLPASSWORD,
