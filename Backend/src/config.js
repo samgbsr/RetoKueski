684adcc8278e33ef8ed2f8ec1e2c75e4e4d46d68
samgbsr <sam.garcia.96@gmail.com>
2023-05-04 19:09:19 -0600

    Commit Railway

---
+++ Backend/src/config.js
@@ -0,0 +1,9 @@
+import {config} from 'dotenv'
+
+config()
+
+export const PORT = process.env.PORT || 3000
+export const MYSQLHOST = process.env.MYSQLHOST || 'localhost'
+export const MYSQLUSER = process.env.MYSQLUSER || 'root'
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || ''
+export const MYSQLDATABASE = process.env.MYSQLDATABASE || 'kueskiarco'
